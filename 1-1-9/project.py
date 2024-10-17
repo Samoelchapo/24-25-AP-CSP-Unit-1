737936afc0d0991ba298aca70d8adfad6ee84b00
20556 <samuel.meneses@vtsd.com>
2024-10-17 13:12:24 -0400

    pythin

---
+++ 1-1-9/project.py
@@ -0,0 +1,39 @@
+import turtle as trtl
+#Semi Circle size
+painter = trtl.Turtle()
+painter.goto(0,0)
+painter.speed(0)
+painter.pensize(5)
+painter.fillcolor('black')
+painter.circle(180,180)
+painter.right(220)
+painter.penup()
+painter.goto(-230,80)
+painter.pendown()
+painter.circle(180,180)
+painter.penup()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
